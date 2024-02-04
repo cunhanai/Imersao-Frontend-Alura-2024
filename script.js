cd9c335c533ce31bdedb231c8a7c8536ba04f036
Ana Cunha <dacunhanajulia@gmail.com>
2024-02-04 11:55:00 -0300

    (Ana Cunha 04/02/2024) feat: Continuando a manipulação de eventos (commitando o que esqueci de commitar antes)

--- script.js
+++ script.js
@@ -1,35 +1,57 @@
-// Variables
+// VARIÁVEIS
 const searchInput = document.getElementById('search-input');
 const resultArtists = document.getElementById('result-artist');
 const resultPlaylist = document.getElementById('result-playlist');
+const anuncioPremium = document.getElementById("anuncio-premium");
 
-// Funcions
+// FUNÇÕES
 function requestApi(searchTerm) {
     const urlBase = 'http://localhost:3000/artists';
     const urlQuery = urlBase + `?name=${encodeURI(searchTerm)}`
 
     fetch(urlQuery)
-        .then((response) => response.json());
-        // .then(result => displayResults(result));
+        .then((response) => response.json())
+        .then(result => displayResults(result));
 }
 
-// function displayResults(result) { 
+function displayResults(result) { 
+    if (result.some(s => s)) {
+        const artistName = document.getElementById('artist-name');
+        const artistImg = document.getElementById('artist-img');
+        
+        result.forEach(element => {
+            artistName.innerText = element.name;
+            artistImg.src = element.urlImg;
+        });
+        
+        resultPlaylist.classList.add('hidden');
+        resultArtists.classList.remove('hidden');
+    }
+    else {
+        cleanResult()
+    }
+}
 
-// }
+function cleanResult() {
+    resultArtists.classList.add('hidden');
+    resultPlaylist.classList.remove('hidden');
+}
 
-// Document listener
+// MANIPULAÇÃO DE EVENTOS
+
+// Pesquisando artista
 document.addEventListener('input', () => {
     const searchTerm = searchInput.value;
 
     if (searchTerm === '') {
-        resultArtists.classList.add('hidden');
-        resultPlaylist.classList.remove('hidden')
+        cleanResult()
         return;
-    }
-    else  {
-        resultPlaylist.classList.add('hidden');
-        resultArtists.classList.remove('hidden')
     }
 
     requestApi(searchTerm);
+})
+
+// Fechando botão
+document.getElementById('close-button').addEventListener("click", () => {
+    anuncioPremium.classList.add('hidden')
 })
